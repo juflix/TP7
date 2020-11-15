b90f49241ce36a328566597604c21a8880ea85d1
Ju <felixjuliette@gmail.com>
2020-11-15 17:31:17 +0100

    ajoute de la classe Point et modifications des coordonnees dans Animal

--- src/Chat.java
+++ src/Chat.java
@@ -1,6 +1,6 @@
 public class Chat extends Animal {
     public void seDeplacer(int x, int y){
-        this.abs += x;
-        this.ord += y;
+        this.coordonnees.abs += x;
+        this.coordonnees.ord += y;
     }
 }
--- src/Chien.java
+++ src/Chien.java
@@ -1,6 +1,6 @@
 public class Chien extends Animal {
     public void seDeplacer(int x, int y){
-        this.abs += x;
-        this.ord += y;
+        this.coordonnees.abs += x;
+        this.coordonnees.ord += y;
     }
 }
