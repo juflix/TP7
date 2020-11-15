6476ad73b930b5d08009b91a56d739ac3cf144f6
Ju <felixjuliette@gmail.com>
2020-11-15 17:03:03 +0100

    implémentatoin de la fonction faireDuBruit pour les Chat et Chien

--- src/Chat.java
+++ src/Chat.java
@@ -1,6 +1,10 @@
-public class Chat extends Animal {
+public class Chat extends Animal implements Bruit{
     public void seDeplacer(int x, int y){
         this.abs += x;
         this.ord += y;
+    }
+
+    public void faireDuBruit(){
+        System.out.println("Je suis un chat miaou!");
     }
 }
--- src/Chien.java
+++ src/Chien.java
@@ -1,6 +1,10 @@
-public class Chien extends Animal {
+public class Chien extends Animal implements Bruit {
     public void seDeplacer(int x, int y){
         this.abs += x;
         this.ord += y;
+    }
+
+    public void faireDuBruit(){
+        System.out.println("Je suis un chien waouf!");
     }
 }
